53cae2b8614c0df532a3045834e8c62888ae2730
Kurama07a <bt22csh005@iiitn.ac.in>
2025-07-16 14:44:51 +0530

    CHORE: cleanup

---
+++ frontend/src/react-jsx-runtime.d.ts
@@ -0,0 +1,8 @@
+// Type declaration for React JSX runtime
+declare module 'react/jsx-runtime' {
+  export * from 'react/jsx-runtime'
+}
+
+declare module 'react/jsx-dev-runtime' {
+  export * from 'react/jsx-dev-runtime'
+}
